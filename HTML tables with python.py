08c05715ad86297c67f64e3963276a2c952d625d
Amine HADJ MELIANI <112354395+AmineHadjmeliani@users.noreply.github.com>
2023-01-24 08:28:41 +0100

    Update HTML tables with python.py

--- HTML tables with python.py
+++ HTML tables with python.py
@@ -2,10 +2,18 @@ import requests
 from bs4 import BeautifulSoup 
 import pandas as pd 
 
+# URL of the Premier League table page on Sky Sports website
 url = "https://www.skysports.com/premier-league-table"
 
-# Pagination 
+# Send GET request to the URL and retrieve the source code
 src = requests.get(url)
+
+# Parse the HTML of the page using BeautifulSoup
 soup = BeautifulSoup(src.text, 'html.parser')
+
+# Use pandas to read the HTML table on the page and create a DataFrame
 df = pd.read_html(url, index_col='#')
+
+# Drop the 'Last 6' column from the DataFrame
 table = df[0].drop(columns='Last 6')
+print(table)
